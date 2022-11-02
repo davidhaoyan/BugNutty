a700ad24956c51850e9ae135fd57427a912e3ba6
David Yan <davidhaoyan@gmail.com>
2022-11-02 15:40:33 +0000

    sprites

---
+++ learn/Sprites.py
@@ -0,0 +1,41 @@
+import pygame
+import math, random
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 750, 750
+
+class Fist(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Fist, self).__init__()
+        """self.image = pygame.Surface((50, 50))
+        self.image.fill((0,0,0))"""
+        self.image = pygame.image.load(r"C:\Users\david\Documents\bug nutty\BugNutty\learn\Clenched_human_fist.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (300, 300))
+        self.rect = self.image.get_rect()
+        self.rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        self.speed = 5
+
+    def update(self):
+        rectX, rectY = self.rect.center
+        mouseX, mouseY = pygame.mouse.get_pos()
+        dy = mouseY - rectY
+        dx = mouseX - rectX
+        angle = math.atan2(dy, dx)
+        self.rect.x += self.speed * math.cos(angle)
+        self.rect.y += self.speed * math.sin(angle)
+        
+    def punch(self):
+        self.rect.x += 20
+        self.rect.y -= 40
+
+    def unpunch(self):
+        self.rect.x -= 20
+        self.rect.y += 40
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.image = pygame.Surface((50,50))
+        self.image.fill((0,0,0))
+        self.rect = self.image.get_rect()
+        self.rect.centerx = random.randint(150, SCREEN_WIDTH-150)
+        self.rect.centery = random.randint(150, SCREEN_HEIGHT-150)
---
+++ learn/main.py
@@ -0,0 +1,46 @@
+import pygame
+import math
+import Sprites
+pygame.init()
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 750, 750
+GREY = (79, 79, 79)
+GREEN = (214, 255, 125)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
+fist = Sprites.Fist()
+Enemies = pygame.sprite.Group()
+
+enemyEvent = pygame.USEREVENT + 1
+pygame.time.set_timer(enemyEvent, 2000)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 3:
+                fist.punch()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 3:
+                fist.unpunch()
+        elif event.type == enemyEvent:
+                enemy = Sprites.Enemy()
+                Enemies.add(enemy)
+    if pygame.mouse.get_pressed()[0]:
+        mouseX, mouseY = pygame.mouse.get_pos()
+        if (abs(mouseX - fist.rect.centerx) > 1) & (abs(mouseY - fist.rect.centery) > 1):
+            fist.update()
+    fist.rect.clamp_ip(screen.get_rect())
+
+    screen.fill(GREY)
+    screen.blit(fist.image, fist.rect)
+    Enemies.draw(screen)
+    pygame.display.flip()
+    clock.tick(60)
+
+
+pygame.quit()
